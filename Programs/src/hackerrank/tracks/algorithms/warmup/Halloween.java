883198bce2be5137d15e41cfb8094448fca7f7a2
shashank <stdio430@live.com>
2014-06-06 13:58:39 +0530

    added package

--- Programs/src/hackerrank/tracks/algorithms/warmup/Halloween.java
+++ Programs/src/hackerrank/tracks/algorithms/warmup/Halloween.java
@@ -1,19 +1,21 @@
+package hackerrank.tracks.algorithms.warmup;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class LoveLetterMystery {
+
+public class Halloween {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 		while(t-->0){
-			String str = br.readLine();
-			char arr[] = str.toCharArray();
-			int res = 0,n=arr.length;
-			for(int i=0;i<n/2;i++){
-				res += Math.abs(arr[i]-arr[n-i-1]);
+			long n = Long.parseLong(br.readLine());
+			if(n%2==0){
+				System.out.println(n/2*n/2);
 			}
-			System.out.println(res);
+			else {
+				System.out.println((n/2)*(n/2+1));
+			}
 		}
 	}
 }
--- Programs/src/hackerrank/tracks/algorithms/warmup/LoveLetterMystery.java
+++ Programs/src/hackerrank/tracks/algorithms/warmup/LoveLetterMystery.java
@@ -1,20 +1,20 @@
+package hackerrank.tracks.algorithms.warmup;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
-public class Halloween {
+public class LoveLetterMystery {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int t = Integer.parseInt(br.readLine());
 		while(t-->0){
-			long n = Long.parseLong(br.readLine());
-			if(n%2==0){
-				System.out.println(n/2*n/2);
+			String str = br.readLine();
+			char arr[] = str.toCharArray();
+			int res = 0,n=arr.length;
+			for(int i=0;i<n/2;i++){
+				res += Math.abs(arr[i]-arr[n-i-1]);
 			}
-			else {
-				System.out.println((n/2)*(n/2+1));
-			}
+			System.out.println(res);
 		}
 	}
 }
